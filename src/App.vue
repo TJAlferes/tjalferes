faefd8edcc7b91992a6221d56b70c0c0b9793765
T. J. Alferes <tjalferes@gmail.com>
2020-03-05 07:22:04 -0600

    new vue

---
+++ src/App.vue
@@ -0,0 +1,54 @@
+<template>
+  <div id="app">
+
+    <header>
+      <div class="header-top">
+        <a href="/index.html" id="current-page">HOME</a>
+        <a href="/biography/biography.html">BIOGRAPHY</a>
+        <a href="/paintings/paintings.html">PAINTINGS</a>
+        <a href="/sites/sites.html">SITES</a>
+        <a href="/articles/articles.html">ARTICLES</a>
+        <a href="/books/books.html">BOOKS</a>
+      </div>
+      <div class="header-bottom">
+        <div class="logo">
+          <span class="website-title">TJALFERES.net</span>
+          <span class="website-subtitle">Website of Timothy James Alferes</span>
+        </div>
+      </div>
+    </header>
+
+    <main>
+      <div id="home">
+        <h1>EDIT</h1>
+      </div>
+    </main>
+
+    <footer>
+      <div id="social-media-links">
+        <a href="https://github.com/TJAlferes">
+          <img src="/images/github-logo-24px.png" alt="GitHub">
+        </a>
+        <a href="https://twitter.com/tjalferes">
+          <img src="/images/twitter-logo-24px.png" alt="Twitter">
+        </a>
+        <a href="https://facebook.com/timothyjamesalferes">
+          <img src="/images/facebook-logo-24px.png" alt="Facebook">
+        </a>
+      </div>
+    </footer>
+    
+  </div>
+</template>
+
+<script lang="ts">
+
+export default {
+  
+};
+
+</script>
+
+<style lang="postcss">
+
+</style>
