502ded9f86ef19607f844d1580059ff7d6e6ee43
T. J. Alferes <tjalferes@gmail.com>
2020-03-04 19:13:59 -0600

    link cleanup

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,5 +1,4 @@
 const path = require('path');
-const webpack = require('webpack');
 const HTMLWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
@@ -8,17 +7,10 @@ module.exports = {
     contentBase: 'dist',
     hot: true,
     overlay: true,
-    stats: {
-      colors: true
-    }
+    stats: {colors: true}
   },
   devtool: 'source-map',
-  entry: {
-    index: [
-      'webpack-hot-middleware/client?reload=true',
-      './src/main.js'
-    ]
-  },
+  entry: {index: './src/main.js'},
   output: {
     filename: '[name]-bundle.js',
     path: path.resolve(__dirname, '../dist'),
@@ -33,51 +25,19 @@ module.exports = {
       },
       {
         test: /\.css$/,
-        use: [
-          {
-            loader: 'style-loader'
-          },
-          {
-            loader: 'css-loader'
-          }
-        ]
+        use: [{loader: 'style-loader'}, {loader: 'css-loader'}]
       },
       {
         test: /\.html$/,
-        use: [
-          {
-            loader: 'html-loader',
-            options: {
-              attrs: ['img:src']
-            }
-          }
-        ]
-      },
-      {
-        test: /\.hbs$/,
-        use: [
-          {
-            loader: 'handlebars-loader'
-          }
-        ]
+        use: [{loader: 'html-loader', options: {attrs: ['img:src']}}]
       },
       {
         test: /\.(png|jpe?g|gif|ico)$/,
-        use: [
-          {
-            loader: 'file-loader',
-            options: {
-              name: 'images/[name].[ext]'
-            }
-          }
-        ]
+        use: [{loader: 'file-loader', options: {name: 'images/[name].[ext]'}}]
       }
     ]
   },
   plugins: [
-    new webpack.HotModuleReplacementPlugin(),
-    new HTMLWebpackPlugin({
-      template: './src/index.html'
-    })
+    new HTMLWebpackPlugin({template: './src/index.html'})
   ]
 };
--- webpack.prod.js
+++ webpack.prod.js
@@ -6,129 +6,52 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCSSExtractPlugin = require('mini-css-extract-plugin');
 const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
 const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
-/*const BundleAnalyzerPlugin = require(
-  'webpack-bundle-analyzer'
-).BundleAnalyzerPlugin;*/
 
 module.exports = {
   mode: 'production',
   //devtool: 'source-map',
-  entry: {
-    main: ['./src/main.js']
-  },
+  entry: {main: ['./src/main.js']},
   output: {
     filename: '[name]-bundle.js',
-    //filename: 'bundle.js',
-    //chunkFilename: '[id].js',
     path: path.resolve(__dirname, 'dist'),
-    //path: path.resolve(__dirname, '../dist'),
     publicPath: '/'
   },
-  optimization: {
-    splitChunks: {
-      chunks: 'all'
-    }
-  },
+  optimization: {splitChunks: {chunks: 'all'}},
   module: {
     rules: [
       {
         test: /\.js$/,
-        exclude: [
-          /node_modules/,
-          path.resolve(__dirname, 'src/server/devServer.js')
-        ],
+        exclude: [/node_modules/],
         loader: 'babel-loader'
       },
       {
         test: /\.css$/,
         use: [
-          //'style-loader',
           MiniCSSExtractPlugin.loader,
-          {
-            loader: 'css-loader',
-            options: {
-              importLoaders: 1
-            }
-          },
+          {loader: 'css-loader', options: {importLoaders: 1}},
           'postcss-loader'
-          /*
-          {
-            loader: 'postcss-loader',
-            options: {
-              ident: 'postcss',
-              plugins: () => [
-                autoprefixer({
-                  browsers: [
-                    "> 1%",
-                    "last 2 versions"
-                  ]
-                })
-              ]
-            }
-          }
-          */
         ]
       },
       {
         test: /\.html$/,
         exclude: path.resolve(__dirname, 'src/index.html'),
-        use: [
-          {
-            loader: 'file-loader',
-            options: {
-              name: '[name].[ext]'
-            }
-          }
-          /*
-          {
-            loader: 'html-loader',
-            options: {
-              attrs: ['img:src']
-            }
-          }
-          */
-        ]
+        use: [{loader: 'file-loader', options: {name: '[name].[ext]'}}]
       },
-      /*
-      {
-        test: /\.hbs$/,
-        use: [
-          {
-            loader: 'handlebars-loader'
-          }
-        ]
-      },
-      */
       {
         test: /\.(png|jpe?g|gif|ico)$/,
-        use: [
-          'url-loader'
-          /*
-          {
-            loader: 'file-loader',
-            options: {
-              name: 'images/[name].[ext]'
-            }
-          }
-          */
-        ]
+        use: ['url-loader']
       }
     ]
   },
   plugins: [
     new CleanPlugin,
     new OptimizeCSSAssetsPlugin,
-    new MiniCSSExtractPlugin({
-      filename: '[name]-[contenthash].css'
-    }),
+    new MiniCSSExtractPlugin({filename: '[name]-[contenthash].css'}),
     new webpack.EnvironmentPlugin(['NODE_ENV']),
-    //new BundleAnalyzerPlugin(),
     new HtmlWebpackPlugin({
       template: __dirname + '/src/index.html',
       inject: true,
-      //template: __dirname + '/public/index.html',
-      filename: 'index.html',
-      //inject: 'body'
+      filename: 'index.html'
     }),
     new UglifyJSPlugin()
   ]
